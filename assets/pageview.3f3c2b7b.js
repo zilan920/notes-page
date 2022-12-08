cb96e33d3934fcd8e5780b3604b33c2f85c7ade1
zilan920 <zilan920@users.noreply.github.com>
2022-12-08 02:33:01 +0000

    Update file(s) "/." from "zilan920/notes"

--- assets/pageview.3f3c2b7b.js
+++
@@ -1,1 +0,0 @@
-import{i as t}from"./app.62eaac2e.js";const n=()=>t({serverURL:"https://waline.newzone.top"});export{n as updatePageview};
---
+++ assets/pageview.663f2ef3.js
@@ -0,0 +1,1 @@
+import{i as t}from"./app.264958ce.js";const n=()=>t({serverURL:"https://waline.newzone.top"});export{n as updatePageview};

899ea800362c1e2deb3d2d0ec7ad2869279721bd
zilan920 <zilan920@users.noreply.github.com>
2022-12-10 04:37:04 +0000

    Update file(s) "/." from "zilan920/notes"

--- assets/pageview.4fe77949.js
+++
@@ -1,1 +0,0 @@
-import{i as t}from"./app.afcee0b7.js";const p=()=>t({serverURL:"https://notes-page-mu.vercel.app/"});export{p as updatePageview};
---
+++ assets/pageview.ceb1ec74.js
@@ -0,0 +1,1 @@
+import{i as t}from"./app.05a028f5.js";const p=()=>t({serverURL:"https://notes-page-mu.vercel.app/"});export{p as updatePageview};

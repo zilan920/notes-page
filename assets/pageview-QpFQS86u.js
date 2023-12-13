94901103becc050a761d8b4a1feb9d1121f2f447
zilan920 <zilan920@users.noreply.github.com>
2023-12-13 07:57:48 +0000

    Update file(s) "/." from "zilan920/notes"

---
+++ assets/pageview-QpFQS86u.js
@@ -0,0 +1,7 @@
+import{_ as r}from"./app-7EATgD7v.js";const o=async()=>{try{const{pageviewCount:e}=await r(()=>import("./pageview_vuepress-plugin-comment2-XPXQPciS.js"),__vite__mapDeps([]));return e({serverURL:COMMENT_OPTIONS.serverURL})}catch{console.error("@waline/client is not installed!");return}};export{o as updatePageview};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
